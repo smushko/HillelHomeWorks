481e4def3dbe49fbd78b1ecfdefbb2140033ede6
smushko <smushko@gmail.com>
2019-12-28 16:54:13 +0200

    22

--- src/Lesson1/task2/Main.java
+++ src/Lesson1/task2/Main.java
@@ -13,29 +13,37 @@ import java.util.OptionalInt;
 public class Main {
     public static void main(String[] args) throws IOException {
         List<String> dataFromFile = null;
-        try {
-            dataFromFile = Files.readAllLines(Paths.get("c:\\Users\\user\\IdeaProjects\\HillelHomeWorks\\src\\Lesson1\\task2\\file.txt"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println("содержимое файла:");
-        dataFromFile.forEach(System.out::println);
-        dataFromFile.forEach(System.out::println);
-
-        OptionalInt max = dataFromFile.stream().mapToInt(String::length).max();
-        System.out.println("\nМаксимальная длинна строки: " + max.getAsInt() + " символов");
-
-        BufferedReader reader = new BufferedReader(new FileReader("c:\\Users\\user\\IdeaProjects\\HillelHomeWorks\\src\\Lesson1\\task2\\file.txt"));
-        // считаем сначала первую строку
-
-        int maxLengthStr = 0;
-        String line = reader.readLine();
-        while (line != null) {
-            if (line.length() > maxLengthStr) {
-                maxLengthStr = line.length();
+        long start0 = System.currentTimeMillis();
+        for (int i = 0; i < 100; i++) {
+            try {
+                dataFromFile = Files.readAllLines(Paths.get("c:\\Users\\user\\IdeaProjects\\HillelHomeWorks\\src\\Lesson1\\task2\\file.txt"));
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            line = reader.readLine();
+//        System.out.println("содержимое файла:");
+            //      dataFromFile.forEach(System.out::println);
+            //    dataFromFile.forEach(System.out::println);
+
+            OptionalInt max = dataFromFile.stream().mapToInt(String::length).max();
         }
+        //System.out.println("\nМаксимальная длинна строки: " + max.getAsInt() + " символов");
+        System.out.println(System.currentTimeMillis() - start0);
+        int maxLengthStr = 0;
+        long start = System.currentTimeMillis();
+
+        for (int i = 0; i < 100; i++) {
+            BufferedReader reader = new BufferedReader(new FileReader("c:\\Users\\user\\IdeaProjects\\HillelHomeWorks\\src\\Lesson1\\task2\\file.txt"));
+            // считаем сначала первую строку
+
+            String line = reader.readLine();
+            while (line != null) {
+                if (line.length() > maxLengthStr) {
+                    maxLengthStr = line.length();
+                }
+                line = reader.readLine();
+            }
+        }
+        System.out.println(System.currentTimeMillis() - start);
         System.out.println("\nМаксимальная длинна строки: " + maxLengthStr + " символов");
     }
 }
